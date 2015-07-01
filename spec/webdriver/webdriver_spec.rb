d81c603a1cac102435a42fa51dad2d82240b0aaa
Oleg <nazarov90@gmail.com>
2015-07-01 15:55:42 +0300

    fix choose_tab, added test

--- spec/webdriver/webdriver_spec.rb
+++ spec/webdriver/webdriver_spec.rb
@@ -2,15 +2,23 @@ require 'rspec'
 require_relative '../../testing_shared'
 
 describe WebDriver do
+  let(:webdriver) { WebDriver.new(:chrome) }
+
   it 'Check for popup open' do
-    webdriver = WebDriver.new(:chrome)
     webdriver.new_tab
     expect(webdriver.tab_count).to eq(2)
   end
 
+  it 'choose_tab' do
+    webdriver.new_tab
+    webdriver.choose_tab(2)
+    expect(webdriver.tab_count).to eq(2)
+  end
+
   it 'Check for browser size' do
-    webdriver = WebDriver.new(:chrome)
     expect(webdriver.browser_size.x).to eq(webdriver.headless.resolution_x)
     expect(webdriver.browser_size.y).to eq(webdriver.headless.resolution_y)
   end
+
+  after { webdriver.quit }
 end
