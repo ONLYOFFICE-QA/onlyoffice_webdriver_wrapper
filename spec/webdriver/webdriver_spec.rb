f5c6c93d3cfaa74c821bf646e90536569175d271
Pavel Lobashov <shockwavenn@gmail.com>
2015-07-01 15:00:37 +0300

    Add failing case for check browser size

--- spec/webdriver/webdriver_spec.rb
+++ spec/webdriver/webdriver_spec.rb
@@ -7,4 +7,10 @@ describe WebDriver do
     webdriver.new_tab
     expect(webdriver.tab_count).to eq(2)
   end
+
+  it 'Check for browser size' do
+    webdriver = WebDriver.new(:chrome)
+    expect(webdriver.browser_size.x).to eq(webdriver.headless.resolution_x)
+    expect(webdriver.browser_size.y).to eq(webdriver.headless.resolution_y)
+  end
 end
