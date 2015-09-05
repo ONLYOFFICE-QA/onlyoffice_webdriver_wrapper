a1d53dd5aa1fc11a4f83d820e5a32144b9b58a09
Your Name <flamine@list.ru>
2015-09-05 14:10:32 +0300

    add screenshot folder before screenshots save and add exeption if keys is not found

--- helpers/webdriver.rb
+++ helpers/webdriver.rb
@@ -541,20 +541,21 @@ class WebDriver
     execute_javascript("$(document.evaluate(\"#{list_xpath.tr("\"", "'")}\", document, null, XPathResult.ANY_TYPE, null).iterateNext()).scrollTop(#{pixels})")
   end
 
-  def get_screenshot_and_upload(path_to_screenshot = "/tmp/#{StringHelper.generate_random_string}.png")
+  def get_screenshot_and_upload(path_to_screenshot = "/tmp/screenshot/#{StringHelper.generate_random_string}.png")
     get_screenshot(path_to_screenshot)
     begin
-      link = AmazonS3Wrapper.new.upload_file_and_make_public(path_to_screenshot, 'screenshots')
-      LoggerHelper.print_to_log("upload screenshot: #{link}")
-    rescue RuntimeError => e
+      path_to_screenshot = AmazonS3Wrapper.new.upload_file_and_make_public(path_to_screenshot, 'screenshots')
+      LoggerHelper.print_to_log("upload screenshot: #{path_to_screenshot}")
+    rescue Errno::ENOENT => e
       LoggerHelper.print_to_log("Cant upload screenshot #{path_to_screenshot}. Error: #{e}")
-      link = path_to_screenshot
     end
-    link
+    path_to_screenshot
   end
 
-  def get_screenshot(path_to_screenshot = "/tmp/#{StringHelper.generate_random_string}.png")
-    FileHelper.create_folder(File.dirname(path_to_screenshot))
+  def get_screenshot(path_to_screenshot = "/tmp/screenshot/#{StringHelper.generate_random_string}.png")
+    begin
+      FileHelper.create_folder(path_to_screenshot.sub(%r{[\/]\w+\.\w*}, ''))
+    end
     @driver.save_screenshot(path_to_screenshot)
     LoggerHelper.print_to_log("get_screenshot(#{path_to_screenshot})")
   end
@@ -1150,7 +1151,7 @@ class WebDriver
 
   def webdriver_screenshot(screenshot_name = StringHelper.generate_random_string(12))
     begin
-      link = get_screenshot_and_upload("/tmp/#{screenshot_name}.png")
+      link = get_screenshot_and_upload("/tmp/screenshot/#{screenshot_name}.png")
     rescue Exception => e
       link = 'An error has occurred!!'
       if @headless.headless_instance.nil?
