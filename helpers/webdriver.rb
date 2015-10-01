b95c6942f93d474ae5e5cc957eb8c9f15c5c6c7e
Your Name <flamine@list.ru>
2015-10-01 17:31:17 +0300

    load screen to data share if error

--- helpers/webdriver.rb
+++ helpers/webdriver.rb
@@ -541,18 +541,19 @@ class WebDriver
     execute_javascript("$(document.evaluate(\"#{list_xpath.tr("\"", "'")}\", document, null, XPathResult.ANY_TYPE, null).iterateNext()).scrollTop(#{pixels})")
   end
 
-  def get_screenshot_and_upload(path_to_screenshot = "/tmp/screenshot/#{StringHelper.generate_random_string}.png")
+  def get_screenshot_and_upload(path_to_screenshot = "/mnt/data_share/screenshot/WebdriverError/#{StringHelper.generate_random_string}.png")
     get_screenshot(path_to_screenshot)
     begin
       path_to_screenshot = AmazonS3Wrapper.new.upload_file_and_make_public(path_to_screenshot, 'screenshots')
       LoggerHelper.print_to_log("upload screenshot: #{path_to_screenshot}")
     rescue Errno::ENOENT => e
+      @driver.save_screenshot(path_to_screenshot)
       LoggerHelper.print_to_log("Cant upload screenshot #{path_to_screenshot}. Error: #{e}")
     end
     path_to_screenshot
   end
 
-  def get_screenshot(path_to_screenshot = "/tmp/screenshot/#{StringHelper.generate_random_string}.png")
+  def get_screenshot(path_to_screenshot = "/mnt/data_share/screenshot/WebdriverError/#{StringHelper.generate_random_string}.png")
     FileHelper.create_folder(File.dirname(path_to_screenshot))
     @driver.save_screenshot(path_to_screenshot)
     LoggerHelper.print_to_log("get_screenshot(#{path_to_screenshot})")
@@ -1149,7 +1150,7 @@ class WebDriver
 
   def webdriver_screenshot(screenshot_name = StringHelper.generate_random_string(12))
     begin
-      link = get_screenshot_and_upload("/tmp/screenshot/#{screenshot_name}.png")
+      link = get_screenshot_and_upload("/mnt/data_share/screenshot/WebdriverError/#{screenshot_name}.png")
     rescue Exception => e
       link = 'An error has occurred!!'
       if @headless.headless_instance.nil?
