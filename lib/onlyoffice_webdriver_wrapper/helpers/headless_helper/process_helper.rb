d7215608e67f0780028343243ab9b636085269ac
Pavel Lobashov <shockwavenn@gmail.com>
2016-11-26 15:55:57 +0300

    Add debug function to look up for proces and ports

--- lib/onlyoffice_webdriver_wrapper/helpers/headless_helper/process_helper.rb
+++ lib/onlyoffice_webdriver_wrapper/helpers/headless_helper/process_helper.rb
@@ -15,5 +15,15 @@ module OnlyofficeWebdriverWrapper
       kill_process('Xvfb')
       kill_process('chrome')
     end
+
+    def show_processes
+      proceses = `ps -ef`
+      OnlyofficeLoggerHelper.log("There is processes on server: #{proceses}")
+    end
+
+    def show_ports
+      ports_usage = `netstat -tulpn`
+      OnlyofficeLoggerHelper.log("There is ports usage on server: #{ports_usage}")
+    end
   end
 end
