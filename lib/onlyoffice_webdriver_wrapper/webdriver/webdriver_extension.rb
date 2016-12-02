0093ea9999f02921487aafe5818b8ddf98b20db6
Pavel Lobashov <shockwavenn@gmail.com>
2016-12-02 11:27:15 +0300

    Increase timeouts for start\stop
    May help fix TimeOutError problem, if not - should remove at all.

--- lib/onlyoffice_webdriver_wrapper/webdriver/webdriver_extension.rb
+++ lib/onlyoffice_webdriver_wrapper/webdriver/webdriver_extension.rb
@@ -1,2 +1,2 @@
-Selenium::WebDriver::Service::START_TIMEOUT = 30
-Selenium::WebDriver::Service::STOP_TIMEOUT = 30
+Selenium::WebDriver::Service::START_TIMEOUT = 120
+Selenium::WebDriver::Service::STOP_TIMEOUT = 120
