472bf0da0e41a11c818885592e4216bedd4607c4
Anatoly Ilyn <anatoly.p6@gmail.com>
2016-12-15 13:18:33 +0300

    Add handling of no jquery case

--- lib/onlyoffice_webdriver_wrapper/webdriver/webdriver_js_methods.rb
+++ lib/onlyoffice_webdriver_wrapper/webdriver/webdriver_js_methods.rb
@@ -27,5 +27,12 @@ module OnlyofficeWebdriverWrapper
       top = execute_javascript("return document.evaluate(\"#{xpath.tr('"', "'")}\",document, null, XPathResult.ANY_TYPE, null ).iterateNext().getBoundingClientRect().top")
       Dimensions.new(left, top)
     end
+
+    # Checks for jQuery finished its job or not present
+    def jquery_finished?
+      jquery_inactive = execute_javascript('return !!window.jQuery && window.jQuery.active;') == 0
+      no_jquery = execute_javascript('return !!window.jQuery') == false
+      jquery_inactive || no_jquery
+    end
   end
 end
