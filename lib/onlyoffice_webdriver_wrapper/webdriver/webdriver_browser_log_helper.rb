7da1606191750f40ad6de9d4e8066556a5a4475f
Pavel Lobashov <shockwavenn@gmail.com>
2018-06-04 14:50:06 +0300

    Do not crash on `Webdrvier#browser_logs` in `firefox`

--- lib/onlyoffice_webdriver_wrapper/webdriver/webdriver_browser_log_helper.rb
+++ lib/onlyoffice_webdriver_wrapper/webdriver/webdriver_browser_log_helper.rb
@@ -2,6 +2,7 @@ module OnlyofficeWebdriverWrapper
   # Methods for working with browser console logs
   module WebdriverBrowserLogHelper
     def browser_logs
+      return [] if browser == :firefox # not implemented, see https://github.com/mozilla/geckodriver/issues/284
       @driver.manage.logs.get(:browser)
     end
   end
