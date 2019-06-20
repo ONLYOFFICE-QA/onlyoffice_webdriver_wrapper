bfce944d70726474e0bf5e00864f67d248dddb06
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-06-20 18:45:11 +0300

    Add test for checking alerts methods (#135)

---
+++ spec/webdriver_alert_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe OnlyofficeWebdriverWrapper::WebDriver do
+  let(:webdriver) { OnlyofficeWebdriverWrapper::WebDriver.new(:chrome) }
+  let(:alert_file) { "#{Dir.pwd}/spec/html_examples/alert.html" }
+  let(:no_alert_file) { "#{Dir.pwd}/spec/html_examples/jquery.html" }
+
+  it 'alert_exists? return true for file with alert' do
+    webdriver.open("file://#{alert_file}")
+    expect(webdriver).to be_alert_exists
+  end
+
+  it 'alert_text return text of alert' do
+    webdriver.open("file://#{alert_file}")
+    expect(webdriver.alert_text).to eq('Hi')
+  end
+
+  it 'alert_exists? return false for file without alert' do
+    webdriver.open("file://#{no_alert_file}")
+    expect(webdriver).not_to be_alert_exists
+  end
+end
