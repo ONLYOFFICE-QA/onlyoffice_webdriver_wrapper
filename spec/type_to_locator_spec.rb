c90d990148e3815adf865f5a36ad88d99209bcfa
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-02-13 19:09:40 +0300

    Add test for checkning type to locator with control (#171)
    It'll fail after update to chromedriver 80

--- spec/type_to_locator_spec.rb
+++ spec/type_to_locator_spec.rb
@@ -5,8 +5,11 @@ require 'rspec'
 describe 'OnlyofficeWebdriverWrapper::WebDriver#type_to_locator' do
   let(:webdriver) { OnlyofficeWebdriverWrapper::WebDriver.new(:chrome) }
 
-  it 'type_to_locator without click enter password in correct field' do
+  before do
     webdriver.open('https://github.com/login')
+  end
+
+  it 'type_to_locator without click enter password in correct field' do
     webdriver.type_to_locator('//*[@id="login_field"]', 'user@example.com')
     webdriver.type_to_locator('//*[@id="password"]', 'password')
     expect(webdriver.get_text('//*[@id="login_field"]')).not_to eq('user@example.com')
@@ -16,6 +19,11 @@ describe 'OnlyofficeWebdriverWrapper::WebDriver#type_to_locator' do
     webdriver.type_to_locator('//*[@id="password"]', 'password', true, true)
     expect(webdriver.get_text('//*[@id="login_field"]')).to eq('user@example.com')
     expect(webdriver.get_text('//*[@id="password"]')).to eq('password')
+  end
+
+  it 'type_to_locator combination with control get no input in locator' do
+    webdriver.type_to_locator('//*[@id="login_field"]', [:control, 'c'])
+    expect(webdriver.get_text('//*[@id="login_field"]')).to be_empty
   end
 
   after { webdriver.quit }
