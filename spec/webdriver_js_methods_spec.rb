bf05acd5fb6ea4011f8951080b73b417e11decab
Pavel Lobashov <shockwavenn@gmail.com>
2020-02-25 18:56:03 +0300

    Add test for `remove_element`
    Move `remove_element` to `WebdriverJsMethods`

--- spec/webdriver_js_methods_spec.rb
+++ spec/webdriver_js_methods_spec.rb
@@ -25,4 +25,11 @@ describe OnlyofficeWebdriverWrapper::WebdriverJsMethods do
                                     ':before',
                                     'content')).to eq('with_pseudo_')
   end
+
+  it 'WebdriverJsMethods#remove_element' do
+    webdriver.open("file://#{pseudo_class_file}")
+    element_to_remove = "//*[@id='first_element']"
+    webdriver.remove_element(element_to_remove)
+    expect(webdriver.get_element(element_to_remove)).to be_nil
+  end
 end
