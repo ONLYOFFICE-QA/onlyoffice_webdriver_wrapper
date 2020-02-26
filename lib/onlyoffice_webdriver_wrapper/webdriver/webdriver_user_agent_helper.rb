eaa6859467cba9e56561b7d43dab5957378c5c1c
Pavel Lobashov <shockwavenn@gmail.com>
2020-02-26 10:54:38 +0300

    Reduce LineLength

--- lib/onlyoffice_webdriver_wrapper/webdriver/webdriver_user_agent_helper.rb
+++ lib/onlyoffice_webdriver_wrapper/webdriver/webdriver_user_agent_helper.rb
@@ -3,10 +3,18 @@
 module OnlyofficeWebdriverWrapper
   # Module for wokring with webdriver useragent
   module WebdriverUserAgentHelper
-    USERAGENT_ANDROID_PHONE = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MDB08M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile Safari/537.36'
-    USERAGENT_IPHONE = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1'
-    USERAGENT_IPAD_AIR_2_SAFARI = 'Mozilla/5.0 (iPad; CPU OS 10_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A5346a Safari/602.1'
-    USERAGENT_NEXUS_10_CHROME = 'Mozilla/5.0 (Linux; Android 4.3; Nexus 10 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Safari/537.36'
+    USERAGENT_ANDROID_PHONE = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MDB08M) '\
+                              'AppleWebKit/537.36 (KHTML, like Gecko) '\
+                              'Chrome/51.0.2704.81 Mobile Safari/537.36'
+    USERAGENT_IPHONE = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) '\
+                       'AppleWebKit/601.1.46 (KHTML, like Gecko) '\
+                       'Version/9.0 Mobile/13F69 Safari/601.1'
+    USERAGENT_IPAD_AIR_2_SAFARI = 'Mozilla/5.0 (iPad; CPU OS 10_0 like Mac OS X) '\
+                                  'AppleWebKit/602.1.50 (KHTML, like Gecko) '\
+                                  'Version/10.0 Mobile/14A5346a Safari/602.1'
+    USERAGENT_NEXUS_10_CHROME = 'Mozilla/5.0 (Linux; Android 4.3; Nexus 10 Build/JSS15Q) '\
+                                'AppleWebKit/537.36 (KHTML, like Gecko) '\
+                                'Chrome/48.0.2564.23 Safari/537.36'
 
     # @return [String] user agent string for current device
     def user_agent_for_device
