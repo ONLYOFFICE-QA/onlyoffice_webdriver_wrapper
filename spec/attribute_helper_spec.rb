92feb63687ad1d5da90e312e4c1c9187351c5078
Pavel Lobashov <shockwavenn@gmail.com>
2020-03-27 15:17:31 +0300

    Add tests for get_attributes_of_several_elements

--- spec/attribute_helper_spec.rb
+++ spec/attribute_helper_spec.rb
@@ -35,5 +35,22 @@ describe 'OnlyofficeWebdriverWrapper::WebDriver#WebdriverAttributesHelper' do
     end
   end
 
+  describe '#get_attributes_of_several_elements' do
+    it 'get_attributes_of_several_elements if elements exists' do
+      expect(webdriver.get_attributes_of_several_elements('//div', 'custom-attribute'))
+        .to eq(%w[first second])
+    end
+
+    it 'get_attributes_of_several_elements if no elements' do
+      expect(webdriver.get_attributes_of_several_elements('//div', 'no-attribute'))
+        .to eq([nil, nil])
+    end
+
+    it 'get_attributes_of_several_elements if not-existin element' do
+      expect(webdriver.get_attributes_of_several_elements('//no', 'no-attribute'))
+        .to be_empty
+    end
+  end
+
   after { webdriver.quit }
 end
