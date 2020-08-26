7e703a8bbdd78bf936ad773e1eb13d3309645fc0
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-08-26 11:00:36 +0300

    Fix `wait_until` test to not to use not-existing localhost page (#236)
    * Fix `wait_until` test to not to use not-existing localhost page
    
    * Fix `wait_until` test to not to use not-existing localhost page

--- spec/onlyoffice_webdriver_wrapper/web_driver/wait_until_spec.rb
+++ spec/onlyoffice_webdriver_wrapper/web_driver/wait_until_spec.rb
@@ -2,13 +2,19 @@
 
 require 'spec_helper'
 
-describe 'wait_until' do
-  let(:webdriver) { OnlyofficeWebdriverWrapper::WebDriver.new(:chrome) }
+describe OnlyofficeWebdriverWrapper::WebDriver, '#wait_until' do
+  let(:webdriver) { described_class.new(:chrome) }
 
   wait_until_message = '`return document.readyState;` with result: `complete`'
 
+  after { webdriver.quit }
+
+  before do
+    webdriver.open("file://#{Dir.pwd}/spec/html_examples/"\
+                      'wait_until.html')
+  end
+
   it 'wait_until by default wait for js load' do
-    webdriver.open('http://127.0.0.1')
     expect do
       webdriver.wait_until do
         true
@@ -17,13 +23,10 @@ describe 'wait_until' do
   end
 
   it 'wait_until wait_js: false will not wait for js load' do
-    webdriver.open('http://127.0.0.1')
     expect do
       webdriver.wait_until(::PageObject.default_page_wait, nil, wait_js: false) do
         true
       end
     end.not_to output(/#{wait_until_message}/).to_stdout
   end
-
-  after { webdriver.quit }
 end
