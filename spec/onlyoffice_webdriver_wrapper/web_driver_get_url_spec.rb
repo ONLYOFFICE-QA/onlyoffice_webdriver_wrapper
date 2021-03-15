59fe48fb59b2292eb63973e7346324fc1113cfb8
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-03-15 19:15:09 +0300

    Do not forever hangup of Timeout Error while `get_url` (#415)
    * Do not forever hangup of Timeout Error while `get_url`
    
    * Revert incorrect exception change

---
+++ spec/onlyoffice_webdriver_wrapper/web_driver_get_url_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe OnlyofficeWebdriverWrapper::WebDriver, '#get_url' do
+  let(:webdriver) { described_class.new(:chrome) }
+
+  it 'Opening url in new tab for ftp site will not cause forever loop' do
+    webdriver.open("file://#{Dir.pwd}/spec/html_examples/"\
+                      'link_to_ftp.html')
+    webdriver.click_on_locator('//a')
+    webdriver.choose_tab(2)
+    expect { webdriver.get_url }.to raise_error(Net::ReadTimeout)
+  end
+end
