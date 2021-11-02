f09dd2b0b7a0a3b02c98d65dbe7da468bfbefde7
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-11-02 13:27:44 +0300

    Use new uploader for `codecov` instead of deprecated one (#638)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'simplecov'
 SimpleCov.start
 
 if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
 require 'onlyoffice_webdriver_wrapper'
