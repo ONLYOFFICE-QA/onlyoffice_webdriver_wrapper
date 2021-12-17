3de7aaddadcc299567efd3ed21cf21634c3d4c7e
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-12-17 10:49:22 +0300

    Release v0.22.1 (#698)

--- lib/onlyoffice_webdriver_wrapper/version.rb
+++ lib/onlyoffice_webdriver_wrapper/version.rb
@@ -4,6 +4,6 @@ module OnlyofficeWebdriverWrapper
   # Module for storing version data
   module Version
     # @return [String] Current stable version of gem
-    STRING = '0.22.0'
+    STRING = '0.22.1'
   end
 end
