51c4ca63658d0e820e194f84c622f568a47b4aac
Pavel Lobashov <shockwavenn@gmail.com>
2022-01-12 12:55:20 +0300

    Remove deprecated parameter

--- lib/onlyoffice_webdriver_wrapper/helpers/chrome_helper.rb
+++ lib/onlyoffice_webdriver_wrapper/helpers/chrome_helper.rb
@@ -35,7 +35,6 @@ module OnlyofficeWebdriverWrapper
         credentials_enable_service: false
       }
       caps = Selenium::WebDriver::Remote::Capabilities.chrome
-      caps[:logging_prefs] = { browser: 'ALL' }
       caps[:proxy] = Selenium::WebDriver::Proxy.new(ssl: "#{@proxy.proxy_address}:#{@proxy.proxy_port}") if @proxy
       switches = add_useragent_to_switches(DEFAULT_CHROME_SWITCHES)
       options = Selenium::WebDriver::Chrome::Options.new(args: switches,
