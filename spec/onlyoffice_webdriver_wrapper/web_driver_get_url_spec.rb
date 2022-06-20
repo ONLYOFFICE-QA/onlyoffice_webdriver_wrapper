7a341b798bb4ee51e9e262799354d32691d84b63
Pavel Lobashov <shockwavenn@gmail.com>
2022-06-20 17:27:23 +0300

    Fix forgotten `quit` command in some specs

--- spec/onlyoffice_webdriver_wrapper/web_driver_get_url_spec.rb
+++ spec/onlyoffice_webdriver_wrapper/web_driver_get_url_spec.rb
@@ -5,6 +5,8 @@ require 'spec_helper'
 describe OnlyofficeWebdriverWrapper::WebDriver, '#get_url' do
   let(:webdriver) { described_class.new(:chrome) }
 
+  after { webdriver.quit }
+
   it 'Opening url in new tab for ftp site will not cause forever loop' do
     webdriver.open(local_file('link_to_ftp.html'))
     webdriver.click_on_locator('//a')
