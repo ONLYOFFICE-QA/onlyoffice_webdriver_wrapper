f41cd6eb362e229e380dd4a01c8048cb20701411
Pavel Lobashov <shockwavenn@gmail.com>
2022-06-20 16:23:54 +0300

    Increase test coverage

--- spec/onlyoffice_webdriver_wrapper/web_driver/move_to_element_by_locator_spec.rb
+++ spec/onlyoffice_webdriver_wrapper/web_driver/move_to_element_by_locator_spec.rb
@@ -2,15 +2,15 @@
 
 require 'spec_helper'
 
-describe OnlyofficeWebdriverWrapper::WebDriver, '#mouse_over' do
+describe OnlyofficeWebdriverWrapper::WebDriver, '#move_to_element_by_locator' do
   let(:webdriver) { described_class.new(:chrome) }
 
   after { webdriver.quit }
 
-  it 'mouse_over simulates mouse over' do
+  it 'move_to_element_by_locator simulates mouse over element' do
     element_to_hover = '//div'
     webdriver.open(local_file('mouse_over.html'))
-    webdriver.mouse_over(element_to_hover)
+    webdriver.move_to_element_by_locator(element_to_hover)
     expect(webdriver.get_style_parameter('//body',
                                          'background'))
       .to eq('rgb(0, 0, 0)')
