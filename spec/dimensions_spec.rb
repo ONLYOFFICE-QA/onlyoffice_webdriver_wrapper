45b5ea0fc782b0abf9e3d18fd354970eb04d5b07
Pavel Lobashov <ShockwaveNN@gmail.com>
2022-07-07 10:07:58 +0300

    Add `Dimesnions#center` method (#909)
    * Add `Dimesnions#center` method
    
    * Remove extra symbols

--- spec/dimensions_spec.rb
+++ spec/dimensions_spec.rb
@@ -7,4 +7,13 @@ describe OnlyofficeWebdriverWrapper::Dimensions do
     expect(described_class.new(50, 100).to_s)
       .to eq('Dimensions(left: 50, top: 100)')
   end
+
+  it 'Dimensions can return center coordinates' do
+    expect(described_class.new(200, 100).center)
+      .to eq(described_class.new(100, 50))
+  end
+
+  it 'Dimensions can be compared with any object' do
+    expect(described_class.new(50, 100)).not_to eq(Object.new)
+  end
 end
