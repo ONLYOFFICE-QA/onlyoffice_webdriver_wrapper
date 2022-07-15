9cddd6472bd6fd311888b1dbbdee7451b64e9219
Pavel Lobashov <shockwavenn@gmail.com>
2022-07-15 17:04:42 +0300

    Extract methods

--- lib/onlyoffice_webdriver_wrapper/webdriver.rb
+++ lib/onlyoffice_webdriver_wrapper/webdriver.rb
@@ -8,6 +8,7 @@ require 'uri'
 require_relative 'helpers/chrome_helper'
 require_relative 'helpers/firefox_helper'
 require_relative 'webdriver/click_methods'
+require_relative 'webdriver/element_getters'
 require_relative 'webdriver/get_text_methods'
 require_relative 'webdriver/select_list_methods'
 require_relative 'webdriver/wait_until_methods'
@@ -95,17 +96,6 @@ module OnlyofficeWebdriverWrapper
       @browser_running = true
     end
 
-    # Get element by it's xpath
-    # @param [String] object_identification xpath of object to find
-    # @return [Object, nil] nil if nothing found
-    def get_element(object_identification)
-      return object_identification unless object_identification.is_a?(String)
-
-      @driver.find_element(:xpath, object_identification)
-    rescue StandardError
-      nil
-    end
-
     # Scroll list to specific element
     # @param [String] list_xpath how to find this list
     # @param [String] element_xpath to which we should scrolled
@@ -172,17 +162,6 @@ module OnlyofficeWebdriverWrapper
       false
     end
 
-    # Get first visible element from several
-    # @param [String] xpath_name to find several objects
-    # @return [Object] first visible element
-    def get_element_by_display(xpath_name)
-      @driver.find_elements(:xpath, xpath_name).each do |element|
-        return element if element.displayed?
-      end
-    rescue Selenium::WebDriver::Error::InvalidSelectorError
-      webdriver_error("get_element_by_display(#{xpath_name}): invalid selector: Unable to locate an element with the xpath expression")
-    end
-
     # Return count of elements (visible and not visible)
     # @param xpath_name [String] xpath to find
     # @param only_visible [True, False] count only visible elements?
@@ -194,22 +173,6 @@ module OnlyofficeWebdriverWrapper
       else
         0
       end
-    end
-
-    # Get array of webdriver object by xpath
-    # @param [String] objects_identification object to find
-    # @param [Boolean] only_visible return invisible if true
-    # @return [Array, Object] list of objects
-    def get_elements(objects_identification, only_visible = true)
-      return objects_identification if objects_identification.is_a?(Array)
-
-      elements = @driver.find_elements(:xpath, objects_identification)
-      if only_visible
-        elements.each do |current|
-          elements.delete(current) unless @browser == :firefox || current.displayed?
-        end
-      end
-      elements
     end
 
     # Check if element visible on page
---
+++ lib/onlyoffice_webdriver_wrapper/webdriver/element_getters.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module OnlyofficeWebdriverWrapper
+  # Methods to get elements
+  module ClickMethods
+    # Get element by it's xpath
+    # @param [String] object_identification xpath of object to find
+    # @return [Object, nil] nil if nothing found
+    def get_element(object_identification)
+      return object_identification unless object_identification.is_a?(String)
+
+      @driver.find_element(:xpath, object_identification)
+    rescue StandardError
+      nil
+    end
+
+    # Get first visible element from several
+    # @param [String] xpath_name to find several objects
+    # @return [Object] first visible element
+    def get_element_by_display(xpath_name)
+      @driver.find_elements(:xpath, xpath_name).each do |element|
+        return element if element.displayed?
+      end
+    rescue Selenium::WebDriver::Error::InvalidSelectorError
+      webdriver_error("get_element_by_display(#{xpath_name}): invalid selector: Unable to locate an element with the xpath expression")
+    end
+
+    # Get array of webdriver object by xpath
+    # @param [String] objects_identification object to find
+    # @param [Boolean] only_visible return invisible if true
+    # @return [Array, Object] list of objects
+    def get_elements(objects_identification, only_visible = true)
+      return objects_identification if objects_identification.is_a?(Array)
+
+      elements = @driver.find_elements(:xpath, objects_identification)
+      if only_visible
+        elements.each do |current|
+          elements.delete(current) unless @browser == :firefox || current.displayed?
+        end
+      end
+      elements
+    end
+  end
+end
