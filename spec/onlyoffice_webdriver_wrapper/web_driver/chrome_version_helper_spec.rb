256c60a45babf5ddfb623eff386fc646832ca60b
OnlyOffice Testing Robot <teamlab.ruby@gmail.com>
2023-05-26 12:04:39 +0300

    Increase code coverage (#1327)
    Fix #1322

--- spec/onlyoffice_webdriver_wrapper/web_driver/chrome_version_helper_spec.rb
+++ spec/onlyoffice_webdriver_wrapper/web_driver/chrome_version_helper_spec.rb
@@ -5,19 +5,29 @@ require 'spec_helper'
 describe OnlyofficeWebdriverWrapper::WebDriver, '#chrome_version_helper' do
   include OnlyofficeWebdriverWrapper::ChromeVersionHelper
 
-  it 'chrome_version return some non-zero major version for default config' do
-    expect(chrome_version.to_s).not_to start_with('0.')
+  describe '#chrome_version' do
+    it 'chrome_version returns some non-zero major version for default config' do
+      expect(chrome_version.to_s).not_to start_with('0.')
+    end
+
+    it 'chrome_version returns 0 version for unknown exe' do
+      expect(chrome_version('fake-exe').to_s).to start_with('0.')
+    end
   end
 
-  it 'chrome_version return 0 version for unknown exe' do
-    expect(chrome_version('fake-exe').to_s).to start_with('0.')
+  describe '#chromedriver_path' do
+    it 'returns path for mac' do
+      expect(chromedriver_path(:mac)).to be_a(String)
+    end
+
+    it 'returns path for linux' do
+      expect(chromedriver_path(:linux)).to be_a(String)
+    end
   end
 
-  it 'chromedriver_path return path for mac' do
-    expect(chromedriver_path(:mac)).to be_a(String)
-  end
-
-  it 'chromedriver_path return path for linux' do
-    expect(chromedriver_path(:linux)).to be_a(String)
+  describe '#chromedriver_path_for_version' do
+    it 'returns default path for old version' do
+      expect(chromedriver_path_for_version(1)).to eq(default_linux)
+    end
   end
 end
