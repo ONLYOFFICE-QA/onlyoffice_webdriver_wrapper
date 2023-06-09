15e9b734883097a2b4fcd86d563626b2c320b977
OnlyOffice Testing Robot <teamlab.ruby@gmail.com>
2023-06-09 17:11:44 +0300

    Add coverage for `action_on_locator_coordinates` (#1359)
    * Add coverage for `action_on_locator_coordinates`
    
    * Fix more

---
+++ spec/onlyoffice_webdriver_wrapper/web_driver/action_on_locator_coordinates_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe OnlyofficeWebdriverWrapper::WebDriver, '#action_on_locator_coordinates' do
+  let(:webdriver) { described_class.new(:chrome) }
+  let(:canvas_xpath) { '//canvas' }
+
+  before do
+    webdriver.open(local_file('action_on_locator_coordinates.html'))
+  end
+
+  after { webdriver.quit }
+
+  it 'Single click on button' do
+    webdriver.action_on_locator_coordinates(canvas_xpath, 50, 50)
+    expect(webdriver.alert_text).to eq('Single Click')
+  end
+
+  it 'Double click on button' do
+    webdriver.action_on_locator_coordinates(canvas_xpath,
+                                            180,
+                                            50,
+                                            :double_click)
+    expect(webdriver.alert_text).to eq('Double Click')
+  end
+end
