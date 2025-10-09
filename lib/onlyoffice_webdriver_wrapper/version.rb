2b8017d6278d58ee16cfe5954c57815db05d0dfc
alinasafonova <53475320+alinasafonova@users.noreply.github.com>
2025-10-09 15:54:44 +0300

    Release v1.39.0 (#2408)
    Co-authored-by: Alina Safonova <absolutecontrol@yandex.ru>

--- lib/onlyoffice_webdriver_wrapper/version.rb
+++ lib/onlyoffice_webdriver_wrapper/version.rb
@@ -4,6 +4,6 @@ module OnlyofficeWebdriverWrapper
   # Module for storing version data
   module Version
     # @return [String] Current stable version of gem
-    STRING = '1.38.0'
+    STRING = '1.39.0'
   end
 end
